6104afe766b8d6d6eb64e74d8dc8850736e427db
Mehrdad <mehrdad.pc@gmail.com>
2025-05-14 11:43:48 +0330

    Seventh commit

--- app/core/config.py
+++ app/core/config.py
@@ -1,4 +1,3 @@
-# core/config.py
 from pydantic_settings import BaseSettings
 
 
@@ -7,7 +6,17 @@ class Settings(BaseSettings):
 
     class Config:
         env_file = "app/core/.env"
-        extra = "ignore"
+        extra = "allow"
+
+# from pydantic import Field
+# from pydantic_settings import SettingsConfigDict
+# class Settings(BaseSettings):
+#     database_url: str = Field(..., env="DATABASE_URL")
+
+#     model_config = SettingsConfigDict(
+#         env_file="app/core/.env",
+#         extra="ignore"
+#     )
 
 
 settings = Settings()
