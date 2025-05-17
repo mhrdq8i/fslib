8ead3c54b0b3e930d4beb94ca22d122733c36a04
Mehrdad <mehrdad.pc@gmail.com>
2025-05-17 18:45:30 +0330

    update code

--- dependencies.py
+++ dependencies.py
@@ -1,5 +1,5 @@
-from jwt import PyJWTError, encode, decode
-from datetime import datetime, timedelta
+from jwt import PyJWTError, decode, encode
+from datetime import datetime, timedelta, timezone
 
 from fastapi import Depends, APIRouter
 from fastapi.security import OAuth2PasswordBearer
@@ -7,7 +7,10 @@ from sqlmodel import select
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 from config import settings
-from exceptions import AuthenticationError, NotSuperuserError
+from exceptions import (
+    AuthenticationError,
+    NotSuperuserError
+)
 from database import get_session
 from models import User
 
@@ -17,40 +20,31 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 router = APIRouter()
 
 
-# async def get_current_user(token: str = Depends(oauth2_scheme)):
-#     try:
-#         # decode returns the payload as a dict
-#         payload = decode(
-#             token,
-#             settings.SECRET_KEY,
-#             algorithms=["HS256"],
-#             options={"require": ["exp", "sub"]},
-#         )
-#         username: str = payload.get("sub")
-
-#         if username is None:
-#             raise credentials_exception
-
-#     except PyJWTError:
-#         raise credentials_exception
-
-#     return {"username": username}
-
 async def get_current_user(
     token: str = Depends(oauth2_scheme),
     session: AsyncSession = Depends(get_session),
 ) -> User:
+
     credentials_exception = AuthenticationError(
-        "Could not validate credentials")
+        "Could not validate credentials"
+    )
     try:
-        payload = decode(token, settings.SECRET_KEY, algorithms=["HS256"])
+        payload = decode(
+            token,
+            settings.SECRET_KEY,
+            algorithms=["HS256"]
+        )
         username: str = payload.get("sub")
         if username is None:
             raise credentials_exception
     except PyJWTError:
         raise credentials_exception
 
-    result = await session.exec(select(User).where(User.username == username))
+    result = await session.exec(
+        select(User).where(
+            User.username == username
+        )
+    )
     user = result.one_or_none()
     if not user:
         raise credentials_exception
@@ -69,12 +63,18 @@ async def get_current_active_superuser(
 def create_access_token(
     data: dict,
     expires_delta: timedelta | None = None
-):
+) -> str:
+    now = datetime.now(timezone.utc)
+    expire = now + (
+        expires_delta or timedelta(
+            minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
+        )
+    )
     to_encode = data.copy()
-    expire = datetime.utcnow() + \
-        (expires_delta or timedelta(
-            minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
-         )
-    to_encode.update({"exp": expire})
+    to_encode.update({"iat": now, "exp": expire})
 
-    return encode(to_encode, settings.SECRET_KEY, algorithm="HS256")
+    return encode(
+        to_encode,
+        settings.SECRET_KEY,
+        algorithm="HS256"
+    )
